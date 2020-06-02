47ee8e3216ff100800df9276f21b0d03951eb7d9
RegeleDavid <regele.david@gmail.com>
2020-06-02 08:45:45 +0200

    elsőmasik_2harmadik

---
+++ ConsoleApplication2/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Class1 : Class2
+    {
+        public Class1()
+        {
+            int a = 2;
+        }
+        public void kiir(String a){
+            Console.WriteLine(this.Szia()+a);
+        }
+    }
+}
---
+++ ConsoleApplication2/Class2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Class2
+    {
+
+        public string Szia()
+        {
+            return "Szia ";
+        }
+    }
+    
+}
