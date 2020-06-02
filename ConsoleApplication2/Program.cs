a130184029d010fee08cfaeaba1ebb54f785f36b
RegeleDavid <regele.david@gmail.com>
2020-06-02 08:47:06 +0200

    mi

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleApplication2
 {
     class Program
     {
         static void Main(string[] args)
         {
-           asdasdas
+            new Class1().kiir("Zsola");
+            Console.ReadKey();
         }
     }
 }
